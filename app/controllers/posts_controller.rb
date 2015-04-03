e693b7a63fd34a0f491c4f349a77266036b89a93
Selene <hakobyan.sn@gmail.com>
2015-04-02 23:12:08 -0700

    Interlude checkpoint

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,7 +21,7 @@ class PostsController < ApplicationController
 
   def create
     @topic = Topic.find(params[:topic_id])
-    @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+    @post = current_user.posts.build(post_params)
       authorize @post
     if @post.save
       flash[:notice] = "Post was saved."
@@ -42,7 +42,7 @@ class PostsController < ApplicationController
     @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
       authorize @post
-    if @post.update_attributes(params.require(:post).permit(:title, :body))
+    if @post.update_attributes(post_params)
       flash[:notice] = "Post was updated."
       redirect_to @post
     else
@@ -50,4 +50,11 @@ class PostsController < ApplicationController
       render :edit
     end
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
 end
