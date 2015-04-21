bacfa670b2d9914c7dc6078c24480bd838fd805a
Selene <hakobyan.sn@gmail.com>
2015-04-20 20:58:39 -0700

    voting checkpoint

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,31 +1,31 @@
 class CommentsController < ApplicationController
   def create
-  	@topic = Topic.find(params[:topic_id])
+  	# @topic = Topic.find(params[:topic_id])
   	@post = Post.find(params[:post_id])
   	@comment = @post.comments.build(comments_params)
   	# @comment.post = @post
   	@comment.user = current_user
   	if @comment.save
   		flash[:notice] = "Comment was saved."
-  		redirect_to [@topic, @post]
+  		redirect_to [@post.topic, @post]
   	else
   		flash[:notice] = "Comment failed to save. Try again."
-  		redirect_to [@topic, @post]
+  		redirect_to [@post.topic, @post]
   	end
   end
 
   def destroy
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.find(params[:post_id])
+    # @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
 
     authorize @comment
      if @comment.destroy
        flash[:notice] = "Comment was removed."
-       redirect_to [@topic, @post]
+       redirect_to [@post.topic, @post]
      else
        flash[:error] = "Comment couldn't be deleted. Try again."
-       redirect_to [@topic, @post]
+       redirect_to [@post.topic, @post]
      end
   end
 
